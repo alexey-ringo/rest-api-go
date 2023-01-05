6ccdc5cc0cede94ecd0c2d1d7edb041b461c8b2b
alexey-ringo <alexey.ringo@gmail.com>
2023-01-06 01:39:59 +0300

    rest api webserver

--- cmd/main/app.go
+++ cmd/main/app.go
@@ -1,25 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net"
 	"net/http"
+	"rest-api-go/internal/user"
 	"time"
 
 	"github.com/julienschmidt/httprouter"
 )
 
-func IndexHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-	name := params.ByName("name")
-	w.Write([]byte(fmt.Sprintf("Hello %s", name)))
+func main() {
+	log.Println("create router")
+	router := httprouter.New()
 
+	log.Println("register user handler")
+	handler := user.NewHandler()
+	handler.Register(router)
+
+	start(router)
 }
 
-func main() {
-	router := httprouter.New()
-	router.GET("/:name", IndexHandler)
-
+func start(router *httprouter.Router) {
+	log.Println("start application")
 	listener, err := net.Listen("tcp", ":1234")
 	if err != nil {
 		panic(err)
@@ -31,5 +34,6 @@ func main() {
 		ReadTimeout:  15 * time.Second,
 	}
 
+	log.Println("server is listening port 0.0.0.0:1234")
 	log.Fatalln(server.Serve(listener))
 }
---
+++ internal/user/handler.go
@@ -0,0 +1,62 @@
+package user
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"rest-api-go/internal/handlers"
+)
+
+// Проверка того, что локальная структура реализует интерфейс
+var _ handlers.Handler = &handler{}
+
+const (
+	usersUrl = "/users"
+	userUrl  = "/users/:uuid"
+)
+
+type handler struct {
+}
+
+// NewHandler Конструктор. Создаем в нем локальную структору, но возвращаем интерфейс
+func NewHandler() handlers.Handler {
+	return &handler{}
+}
+
+func (h *handler) Register(router *httprouter.Router) {
+	router.GET(usersUrl, h.GetList)
+	router.POST(usersUrl, h.CreateUser)
+	router.GET(userUrl, h.GetUserByUUID)
+	router.PUT(userUrl, h.UpdateUser)
+	router.PATCH(userUrl, h.PartiallyUpdateUser)
+	router.DELETE(userUrl, h.DeleteUser)
+}
+
+func (h *handler) GetList(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(200)
+	w.Write([]byte("This is list of users"))
+}
+
+func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(201)
+	w.Write([]byte("This is create user"))
+}
+
+func (h *handler) GetUserByUUID(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(200)
+	w.Write([]byte("This is get user by uuid"))
+}
+
+func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
+	w.Write([]byte("This is update user"))
+}
+
+func (h *handler) PartiallyUpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
+	w.Write([]byte("This is partially update user"))
+}
+
+func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
+	w.Write([]byte("This is delete user"))
+}
